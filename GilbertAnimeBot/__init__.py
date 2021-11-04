d3ba2fd4251d44bc2a529a19f769903c853a9aa1
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-11-04 20:57:15 +0530

    Update __init__.py

--- GilbertAnimeBot/__init__.py
+++ GilbertAnimeBot/__init__.py
@@ -87,6 +87,7 @@ if ENV:
     TIME_API_KEY = os.environ.get('TIME_API_KEY', None)
     AI_API_KEY = os.environ.get('AI_API_KEY', None)
     WALL_API = os.environ.get('WALL_API', None)
+    REM_BG_API_KEY = os.environ.get('REM_BG_API_KEY', None)
     SUPPORT_CHAT = os.environ.get('SUPPORT_CHAT', None)
     SPAMWATCH_SUPPORT_CHAT = os.environ.get('SPAMWATCH_SUPPORT_CHAT', None)
     SPAMWATCH_API = os.environ.get('SPAMWATCH_API', None)
@@ -164,6 +165,7 @@ else:
     TIME_API_KEY = Config.TIME_API_KEY
     AI_API_KEY = Config.AI_API_KEY
     WALL_API = Config.WALL_API
+    REM_BG_API_KEY = Config.REM_BG_API_KEY
     SUPPORT_CHAT = Config.SUPPORT_CHAT
     SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
     SPAMWATCH_API = Config.SPAMWATCH_API
